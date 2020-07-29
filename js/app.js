23ddb550faa1b983d4dcdde481c7f86b602ef3ed
Karan Shetty <shettykaran2110@gmail.com>
2020-07-29 14:00:14 +0530

    Added animation on popup

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 const popup = document.querySelector('.popup');
+const popupContent = document.querySelector('.popup__content');
+const popupClose = document.querySelector('.popup__close');
 const cardContainer = document.querySelector('.card-container');
 
 cardContainer.addEventListener('click', on);
-popup.addEventListener('click', off);
+popupClose.addEventListener('click', off);
 
 function on(e) {
   if (e.target.classList.contains('popup-btn')) {
+    popupContent.style.transform = 'translate(-50%, -50%) scale(1)';
     popup.style.opacity = 1;
     popup.style.visibility = 'visible';
   }
@@ -16,4 +19,5 @@ function on(e) {
 function off() {
   popup.style.opacity = 0;
   popup.style.visibility = 'hidden';
+  popupContent.style.transform = 'translate(-50%, -50%) scale(0.3)';
 }
